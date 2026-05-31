c41bc6cdc934472189c2653be2397502040f7154
emphiasarmlorn <77542294+emphiasarmlorn@users.noreply.github.com>
2026-05-31 19:35:15 +0530

    Add ALU functionality and overflow/carry logic
    Implement arithmetic and logical operations with overflow and carry detection.

--- Week 2/Assignment/alu.v
+++ Week 2/Assignment/alu.v
@@ -10,4 +10,42 @@ module alu(
     output reg       carry,
     output reg       overflow
 );
+
+assign zero = (result == 8'b0); 
+
+always @(*) begin
+	carry = 0;
+	overflow = 0;
+
+	case(op)
+	
+		3'b000 : begin
+		{carry, result} = {1'b0, a} + {1'b0, b};
+		overflow = (~a[7] & ~b[7] &  result[7])
+                     | ( a[7] &  b[7] & ~result[7]);
+		end
+		
+		3'b001 : begin
+		{carry, result} = {1'b0, a} - {1'b0, b};
+		overflow =  (~a[7] & b[7] &  result[7])
+                     | ( a[7] &  ~b[7] & ~result[7]);
+		end
+		
+		3'b010 : result = a & b;
+		3'b011 : result = a | b;
+		3'b100 : result = a ^ b;
+		
+		3'b101 : begin
+		carry = a[7];
+		result = a << 1;
+		end
+		
+		3'b110 : begin
+		carry = a[0];
+		result = a >> 1;
+		end
+	
+	endcase
+end
+
 endmodule
